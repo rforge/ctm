b299735b74531c6ee59c4ea94b552d680032466b
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-03 14:28:16 +0000

    more checks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@110 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/tests/surv-Ex.R
+++ pkg/mlt/tests/surv-Ex.R
@@ -1,13 +1,41 @@
 
 library("mlt")
+set.seed(29)
 
-x <- rexp(100)
-sr <- Surv(x, sample(c(TRUE, FALSE), length(x), replace = TRUE))
-sl <- Surv(x, sample(c(TRUE, FALSE), length(x), replace = TRUE), type = "left")
-si <- Surv(x, x + 1, sample(0:3, length(x), replace = TRUE), type = "interval")
+y <- rexp(10)
+d <- rep(c(TRUE, FALSE), length = length(y))
+### right
+(s <- Surv(y, d, type = "right"))
+attr(s, "type")
+cbind(unclass(s), mlt:::.Surv2matrix(s))
 
-mlt:::.Surv2matrix(sr)
-mlt:::.Surv2matrix(sl)
-mlt:::.Surv2matrix(si)
+### right censored, left truncated
+(s <- Surv(y, y + 1, d))
+attr(s, "type")
+cbind(unclass(s), mlt:::.Surv2matrix(s))
+
+### left censored
+(s <- Surv(y, d, type = "left"))
+attr(s, "type")
+cbind(unclass(s), mlt:::.Surv2matrix(s))
+
+### interval
+dd <- rep(0:3, length = length(y))
+(s <- Surv(y, y + 1, dd, type = "interval"))
+attr(s, "type")
+cbind(unclass(s), mlt:::.Surv2matrix(s))
+
+### interval2
+(s <- Surv(y, y + 1, type = "interval2"))
+attr(s, "type")
+cbind(unclass(s), mlt:::.Surv2matrix(s))
+
+(s <- Surv(y, ifelse(d, Inf, y + 1), type = "interval2"))
+attr(s, "type")
+cbind(unclass(s), mlt:::.Surv2matrix(s))
+
+(s <- Surv(y, ifelse(d, NA, y + 1), type = "interval2"))
+attr(s, "type")
+cbind(unclass(s), mlt:::.Surv2matrix(s))
 
 
