2c2d61588802114594432481380bd5efcce0cbf3
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-03 13:15:10 +0000

    deal with counting, interval2
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@106 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/R/surv.R
+++ pkg/mlt/R/surv.R
@@ -2,7 +2,8 @@
 .Surv2matrix <- function(object) {
 
     type <- attr(object, "type")
-    stopifnot(type %in% c("left", "right", "interval"))
+    stopifnot(type %in% c("left", "right", "interval", 
+                          "interval2", "counting"))
     status <- object[, "status"]
 
     ret <- switch(type,
@@ -10,21 +11,32 @@
                         right = ifelse(status == 1, NA, Inf)),
         "left" = cbind(left = ifelse(status == 1, NA, -Inf),
                        right = object[, "time"]),
+        "interval2" = {
+            ret <- cbind(left = object[, "time1"], 
+                         right = object[, "time2"])
+            ret$left[is.na(ret$left)] <- -Inf
+            ret$right[is.na(ret$right)] <- Inf
+        },
         "interval" = {
-            status <- factor(object[, "status"], levels = 0:3, 
+            status <- factor(status, levels = 0:3, 
                              labels = c("right", "exact", "left", "interval"))
             tmp <- matrix(NA, nrow = nrow(object), ncol = 2)
             colnames(tmp) <- c("left", "right")
             for (s in levels(status)) {
                 idx <- which(status == s)
                 tmp[idx, ] <- switch(s, 
-                    "left" = cbind(-Inf, object[idx, "time1"]),
-                    "exact" = cbind(object[idx, "time1"], NA),
                     "right" = cbind(object[idx, "time1"], Inf),
+                    "exact" = cbind(object[idx, "time1"], NA),
+                    "left" = cbind(-Inf, object[idx, "time1"]),
                     "interval" = object[idx, c("time1", "time2")])
             }
             return(tmp)
-        }
+        },
+        ### left truncation, right censoring
+        "counting" = cbind(left = object[, "stop"],
+                           right = ifelse(status == 1, NA, Inf),
+                           lefttrunc = object[, "start"])
     )
+    ### right truncation, left censoring? truncation and interval?
     ret
 }
