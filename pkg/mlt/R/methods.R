348b0cc7e655624cb5fee8ee1a7f6b692f048aac
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-05 15:08:43 +0000

    reimplement predict and samplefrom
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@126 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/R/methods.R
+++ pkg/mlt/R/methods.R
@@ -15,21 +15,16 @@ logLik.mlt <- function(object, ...)
 estfun.mlt <- function(object, ...)
     -object$score(coef(object, fixed = FALSE))
 
-predict.mlt <- function(object, newdata = NULL, ...) {
+predict.model <- function(object, newdata, coef, ...) {
 
-    if (is.null(newdata)) {
-        if (length(varnames(object$model)) == 1)
-            newdata <- data.frame(const = 1)
-        else 
-            newdata <- object$data
-    }
+    stopifnot(is.data.frame(newdata))
     lapply(1:nrow(newdata), function(i) {
-        ret <- function(y, type = c("trafo", "prob"), ...) {
+        ret <- function(y, type = c("trafo", "prob"), ...) { ### density, quantile, hazard, cumhaz
             type <- match.arg(type)
             data <- newdata[rep(i, length(y)),,drop = FALSE]
             data[[object$response]] <- y
-            Y <- model.matrix(object$model, data = data, ...)
-            ret <- drop(Y %*% coef(object))
+            Y <- model.matrix(object, data = data, ...)
+            ret <- drop(Y %*% coef)
             switch(type, 
                    "trafo" = ret,
                    "prob" = object$todistr$p(ret))
@@ -40,36 +35,68 @@ predict.mlt <- function(object, newdata = NULL, ...) {
     })
 }
 
+predict.mlt <- function(object, newdata = NULL, ...) {
+
+    if (is.null(newdata)) {
+        if (length(varnames(object)) == 1)
+            newdata <- data.frame(const = 1)
+        else 
+            newdata <- object$data
+    }
+    predict(object$model, newdata = newdata, 
+            coef = coef(object, fixed = TRUE), ...)
+}
+
+
 samplefrom <- function(object, ...)
     UseMethod("samplefrom")
 
-samplefrom.mlt <- function(object, newdata = NULL, n = 1, ngrid = 100, 
-                           ...) {
+samplefrom.model <- function(object, newdata = NULL, coef, n = 1, ngrid = 10, ny = 100, ...) {
 
     response <- object$response
-    if (is.null(newdata)) newdata <- object$data
-    if (is.list(newdata)) {
-        newdata[[response]] <- NULL
-        newdata <- do.call("expand.grid", newdata)
-    }
-    ygrid <- generate(object$model, n = ngrid)[[response]]
-    if (is.factor(ygrid)) ygrid <- ygrid[-length(ygrid)]
-    pfun <- predict(object, newdata = newdata)
 
-    idx <- do.call("c", lapply(pfun, function(f) {
-        p <- f(ygrid, type = "prob")
-        pOK <- p[!duplicated(p)]
-        pOK <- pOK[pOK > 0 & pOK < 1]
-        unclass(cut(runif(n), breaks = c(0, pOK, 1))) + sum(p < min(pOK))
-    }))
-    if (is.factor(ygrid)) {
-        ret <- ygrid[idx]
-    } else { ### or invert linear interpolation!!!
-        ret <- data.frame(left = c(-Inf, ygrid, Inf)[idx],
-                          right = c(-Inf, ygrid, Inf)[idx + 1])
+    if (is.null(newdata)) {
+        simdata <- generate(object$model, n = ngrid)
+        if (storage.mode(y) == "double")
+            y <- seq(from = min(y), to = max(y), length = ny)
+        if (is.factor(y)) y <- sort(y)[-nlevels(y)]
+        simdata[[response]] <- y
+        newdata <- simdata
+        newdata[[response]] <- NULL
+        newdata <- expand.grid(newdata)
+    } else {
+        y <- generate(object$model, n = ny)[[response]]
+        if (is.factor(y)) y <- sort(y)[-nlevels(y)]
+        simdata <- expand.grid(y = y, id = 1:nrow(newdata))
+        simdata <- cbind(simdata, newdata[simdata$id,,drop = FALSE])
+        simdata <- as.data.frame(simdata)
     }
-    newdata <- newdata[rep(1:nrow(newdata), rep(n, nrow(newdata))),,drop = FALSE]
-    newdata[[object$response]] <- ret
-    newdata
+
+    p <- object$todistr$p(predict(object$model, newdata = simdata, coef = coef, ...))
+    ny <- length(y)
+    nobs <- nrow(newdata)
+    p <- matrix(p, nrow = nobs, ncol = ny, byrow = TRUE)
+    p <- cbind(0, p, 1)
+
+    .sample <- function(i) {
+        idx <- rowSums(p <= (u <- runif(nobs)))
+        if (storage.mode(y) != "double")
+            return(y[idx])
+        ptmp <- cbind(p[cbind(1:nrow(p), idx)], p[cbind(1:nrow(p), idx + 1)])
+        beta <- (ptmp[,2] - ptmp[,1]) / (y[idx + 1] - y[idx])
+        alpha <- ptmp[,1] - beta * y[idx]
+        return((u - alpha) / beta)
+    }
+    ret <- unlist(lapply(1:n, .sample))
+    newdata <- newdata[rep(1:nrow(newdata), n),,drop = FALSE]
+    newdata[[response]] <- ret
+    return(newdata)
+}
+
+samplefrom.mlt <- function(object, newdata = NULL, n = 1, ...) {
+
+    if (is.null(newdata)) newdata <- object$data
+    samplefrom(object$model, newdata = newdata, 
+               coef = coef(object, fixed = TRUE), n = n, ...)
 }
 
