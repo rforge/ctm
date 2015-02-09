1b9c3c984240335cfc9cf11de823f67b0d531da4
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-09 19:58:55 +0000

    confint for curves
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@143 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/tests/faithful.R
+++ pkg/mlt/tests/faithful.R
@@ -23,6 +23,7 @@ lines(ecdf(faithful$waiting))
 plot(aic)
 
 o <- which.min(aic) + 1
+o <- 6
 Bs <- Bernstein_basis(order = o, var = "waiting", ui = "incre",
                       support = range(faithful$waiting) + c(-5, 5))
 m <- model(Bs)
@@ -52,3 +53,31 @@ p <- 1:99 / 100
 
 plot(p, d[[1]](p, type = "quantile"))
 lines(p, quantile(faithful$waiting, p))
+
+o <- 6
+Bs <- Bernstein_basis(order = o, var = "waiting", ui = "incre",
+                      support = range(faithful$waiting) + c(-5, 5))
+m <- model(Bs)
+mod <- mlt(m, data = faithful)
+
+library("multcomp")
+
+mp <- parm(coef(mod), vcov(mod))
+y <- generate(mod, 50)$waiting
+mc <- confint(glht(mp, linfct = model.matrix(mod$model, 
+    data = data.frame(waiting = y))))
+umc <- confint(glht(mp, linfct = model.matrix(mod$model, 
+    data = data.frame(waiting = y))), calpha = qnorm(.975))
+p <- mod$model$todistr$p
+plot(y, p(mc$confint[, "Estimate"]), type = "l")
+lines(y, p(mc$confint[, "lwr"]))
+lines(y, p(mc$confint[, "upr"]))
+lines(y, p(umc$confint[, "lwr"]))
+lines(y, p(umc$confint[, "upr"]))
+
+library("survival")
+cm <- coxph(Surv(waiting, rep(TRUE, nrow(faithful))) ~ 1, data = faithful)
+plot(survfit(cm))
+lines(y, 1 - p(mc$confint[, "Estimate"]), col = "red")
+lines(y, 1 - p(mc$confint[, "lwr"]), col = "red")
+lines(y, 1 - p(mc$confint[, "upr"]), col = "red")
