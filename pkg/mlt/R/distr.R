49ed91c8856e192d8a10387518ece4c15cec8706
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-17 11:27:52 +0000

    add 2nd deriv of densities
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@150 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/R/distr.R
+++ pkg/mlt/R/distr.R
@@ -2,12 +2,17 @@
 .Normal <- function()
     list(p = pnorm, d = dnorm, 
          ### see also MiscTools::ddnorm
-         dd = function(x) -dnorm(x = x) * x)
+         dd = function(x) -dnorm(x = x) * x,
+         ddd = function(x) dnorm(x = x) * (x^2 - 1))
 
 .Logistic <- function()
-    list(p = plogis, d = dlogis, dd = function(x)
-         (2 * exp(-x)^2 / (1 + exp(-x))^3) - 
-          (exp(-x) / (1 + exp(-x))^2))
+    list(p = plogis, d = dlogis, 
+         dd = function(x) 
+             (exp(x) - exp(2*x)) / (1 + exp(x))^3,
+         ddd = function(x) 
+             (exp(x) - 4*(exp(2*x)) + exp(3*x)) / (1 + exp(x))^4)
+#         (2 * exp(-x)^2 / (1 + exp(-x))^3) - 
+#          (exp(-x) / (1 + exp(-x))^2))
 
 .MinExtrVal <- function()
     list(p = function(x) 1 - exp(-exp(x)),
@@ -17,7 +22,9 @@
              ret
          },
          dd = function(x)
-             exp(x - exp(x)) * (1 - exp(x)))
+             (exp(x) - exp(2*x)) / exp(exp(x)),
+         ddd = function(x)
+             (exp(x) - 3*exp(2*x) + exp(3*x)) / exp(exp(x)))
 
 .distr <- function(which = c("Normal", "Logistic", 
                              "MinExtrVal")) {
