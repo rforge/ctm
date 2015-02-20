e1b09d6077c433eaa5bdc467a643ee8739697268
lmoest <lmoest@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-20 08:59:55 +0000

    add right-truncation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@163 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/tests/truncreg-Ex.R
+++ pkg/mlt/tests/truncreg-Ex.R
@@ -2,6 +2,8 @@
 library("mlt")
 library("truncreg")
 
+
+## Left-truncated 
 set.seed(29)
 n <- 1000	
 x <- runif(n, max = 2 * pi)
@@ -30,3 +32,37 @@ vcov(mltmod)
 #library("numDeriv")
 
 solve(numDeriv::hessian(mltmod$loglik, coef(mltmod)))
+
+
+## right-truncated
+
+set.seed(29)
+n <- 1000	
+x <- runif(n, max = 2 * pi)
+y <- rnorm(n, 2*x + 1, .25)
+d <- data.frame(y = y, x = x)
+## truncated response
+tr <- 11
+d$yt <- ifelse(d$y < tr, d$y, NA)
+d$trunc_right <- tr
+
+tmod <- truncreg(yt ~ x, data = d, point = tr, direction = "right")
+coef(tmod)
+logLik(tmod)
+vcov(tmod)
+
+## MLT
+fm <- as.formula("~ x")
+yb <- polynomial_basis(c(TRUE, TRUE), var = "yt", ci = c(-Inf, 0))
+m <- model(yb, shifting = fm, todistr = "Normal")
+mltmod <- mlt(m, data = d[!is.na(d$yt),,drop = FALSE], trunc = c("right" = "trunc_right"))
+(cf <- coef(mltmod))
+c(-cf[1] / cf[2], -cf[3] / cf[2], 1 / cf[2])
+logLik(mltmod)
+vcov(mltmod)
+
+#library("numDeriv")
+
+solve(numDeriv::hessian(mltmod$loglik, coef(mltmod)))
+
+
