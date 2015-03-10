1b43ac8844aa3224fde4c283f9117e15db4756a4
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-03-10 14:01:37 +0000

    simplify and treat the varying coef case in basefun
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@179 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/R/models.R
+++ pkg/mlt/R/models.R
@@ -1,70 +1,39 @@
 
 model <- function(response, interacting = NULL, shifting = NULL, 
-                  remove_intercept = FALSE,
                   todistr = c("Normal", "Logistic", "MinExtrVal")) {
 
     ### generate() will not work due to missing data
     if (.is.formula(response)) 
         response <- as.basis(response)
     if (.is.formula(interacting)) 
-        interacting <- as.basis(interacting, 
-                                remove_intercept = !remove_intercept)
+        interacting <- as.basis(interacting)
     if (.is.formula(shifting)) 
         shifting <- as.basis(shifting, remove_intercept = TRUE)
 
     if (is.character(todistr))
         todistr <- .distr(todistr)
 
-    if (!is.null(interacting)) {
-        if (!(varnames(response) %in% varnames(interacting)))
-            interacting <- b(iresponse = response, iinteracting = interacting)
-    }
+    if (!is.null(interacting))
+        interacting <- b(iresponse = response, iinteracting = interacting, 
+                         sumconstr = inherits(interacting, "formula_basis"))
 
     if (is.null(interacting) && is.null(shifting)) {
-        stopifnot(!remove_intercept)
         mod <- c(bresponse = response)
     } else if (!is.null(interacting) && is.null(shifting)) {
-        if (remove_intercept)
-            mod <- c(binteracting = interacting)
-        else
-            mod <- c(bresponse = response, binteracting = interacting)
+        mod <- c(binteracting = interacting)
     } else if (is.null(interacting) && !is.null(shifting)) {
-        stopifnot(!remove_intercept)
         mod <- c(bresponse = response, bshifting = shifting)
     } else {
-        if (remove_intercept)
-            mod <- c(binteracting = interacting, bshifting = shifting)
-        else
-            mod <- c(bresponse = response, binteracting = interacting, 
-                     bshifting = shifting)
+        mod <- c(binteracting = interacting, bshifting = shifting)
     }
     ret <- list(model = mod, response = varnames(response), 
-                todistr = todistr, remove_intercept = remove_intercept)
+                todistr = todistr)
     class(ret) <- "model"
     return(ret)
 }
 
-model.matrix.model <- function(object, data, ...) {
-    if (is.null(object$model$binteracting) ||
-        is.null(object$model$bresponse))
-        return(model.matrix(object$model, data = data, ...))
-    X <- model.matrix(object$model, data = data, ...)
-    ui <- attr(X, "constraint")$ui
-    ci <- attr(X, "constraint")$ci
-    Xy <- model.matrix(object$model$bresponse, data = data, ...)
-    uiy <- attr(Xy, "constraint")$ui
-    ciy <- attr(Xy, "constraint")$ci
-    if (!any(is.finite(ciy))) return(X)
-    uiy <- uiy[is.finite(ciy),,drop = FALSE]
-    a <- attr(X, "Assign")
-    i <- grep("bresponse", apply(a, 2, paste, collapse = "-"))
-    ui[is.finite(ci),i] <- uiy[rep(1:nrow(uiy), sum(is.finite(ci)) / nrow(uiy)),]
-    attr(X, "constraint") <- list(ui = ui, 
-        ci = attr(X, "constraint")$ci)
-    X
-}
-
-
+model.matrix.model <- function(object, data, ...)
+    return(model.matrix(object$model, data = data, ...))
 
 varnames.model <- function(x)
     varnames(x$model)
