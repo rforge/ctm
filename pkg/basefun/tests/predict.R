85557c9ea79203ab5b3376ae188ca28ce74c44ba
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-04-15 08:37:19 +0000

    more tests for dimensions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@198 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/basefun/tests/predict.R
+++ pkg/basefun/tests/predict.R
@@ -15,26 +15,33 @@ stopifnot(nrow(X) == nrow(d))
 p <- predict(cb, newdata = d, coef = rep(1, ncol(X)))
 stopifnot(length(p) == nrow(d))
 
-(p2 <- predict(cb, newdata = list(x = x, y = y, g = g), coef = rep(1, ncol(X))))
-
-stopifnot(all.equal(p, c(p2), check.attributes = FALSE))
-
-(p3 <- predict(cb, newdata = list(logx = list(x = x), X = expand.grid(y = y, g = g)), 
+(p2 <- predict(cb, newdata = list(x = x, y = y, g = g), 
                coef = rep(1, ncol(X))))
 
-stopifnot(all.equal(p, c(p3), check.attributes = FALSE))
+stopifnot(all.equal(p, c(p2), check.attributes = FALSE))
 
 (p4 <- predict(cb, newdata = generate(cb, 4), coef = rep(1, ncol(X))))
 
 stopifnot(all.equal(p, c(p4), check.attributes = FALSE))
 
-XX <- model.matrix(cb[["X"]], data = list(y = y, g = g))
+p <- predict(cb, newdata = expand.grid(g = g, x = x, y = y), 
+             coef = rep(1, ncol(X)))
+
+(p2 <- predict(cb, newdata = list(x = x, y = y, g = g), 
+               coef = rep(1, ncol(X)),
+               dim = c(g = length(g), x = length(x), y = length(y))))
+	
+stopifnot(all.equal(p, c(p2), check.attributes = FALSE))
+
+XX <- model.matrix(cb[["X"]], data = list(y = y, g = g), 
+                   dim = c(y = length(y), g = length(g)))
 
 pX <- predict(cb[["X"]], newdata = list(y = y, g = g), coef = rep(1, ncol(X) - 1))
 
-pX2 <- predict(cb[["X"]], newdata = expand.grid(y = y, g = g), coef = rep(1, ncol(X) - 1))
+pX2 <- predict(cb[["X"]], newdata = expand.grid(y = y, g = g), 
+               coef = rep(1, ncol(X) - 1))
 
-stopifnot(all.equal(pX, c(pX2), check.attributes = FALSE))
+stopifnot(all.equal(c(pX), c(pX2), check.attributes = FALSE))
 
 bb <- b(logx = log_basis(x, varname = "x"),
         X = as.basis(~ y + g, data = expand.grid(y = y, g = g)))
