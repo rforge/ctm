739bb7a35d1e0a92b3668dbed7209dfa0e06c94f
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-07-16 11:27:59 +0000

    Legendre -> Bernstein
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@253 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/basefun/src/L2B.c
@@ -0,0 +1,44 @@
+
+#include <R.h>
+#include <Rmath.h>
+#include <Rinternals.h>
+#include <Rdefines.h>
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+/*
+  convert coefficients of Legendre polynomial on [0, 1] (!!!) to
+  coefficients of Bernstein polynomial of the same degree on [0, 1]
+  
+      Bernstein coef = L2B(degree) %*% Legendre coef
+
+  Farouki (2000) J Comp Appl Math 119 145-160, page 151 (NOT formula 20)
+      
+*/
+
+SEXP L2B(const SEXP degree) {
+
+    SEXP ans;
+    int n, i, j, k, idx;
+    double *ret;
+    
+    n = INTEGER(degree)[0];
+    
+    PROTECT(ans = allocMatrix(REALSXP, n + 1, n + 1));
+    ret = REAL(ans);
+    
+    for (j = 0; j <= n; j++) {
+        for (k = 0; k <= n; k++) {
+            idx = k * (n + 1) + j;
+            ret[idx] = 0.0;
+            for (i = MAX(0, j + k - n); i <= MIN(j, k); i++)
+                ret[idx] += pow(-1, k + i) * choose((double) j, (double) i) * 
+                                             choose((double) k, (double) i) * 
+                                             choose((double) n - j, (double) k - i);
+            ret[idx] = ret[idx] / choose((double) n, (double) k);
+        }
+    }
+    UNPROTECT(1);
+    return(ans);
+}
