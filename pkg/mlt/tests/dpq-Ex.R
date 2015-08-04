37ac47e1c35c0bccbca08499e6089e367ee8f09d
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-04 08:31:19 +0000

    call predict() only
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@283 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/tests/dpq-Ex.R
+++ pkg/mlt/tests/dpq-Ex.R
@@ -11,12 +11,12 @@ m <- model(polynomial_basis(c(TRUE, TRUE), varname = "y",
            shift = ~ x1 + x2)
 mod <- mlt(m, data = d)
 
-p <- tmlt(mod, newdata = d)
+p <- predict(mod, newdata = d)
 
 (p0 <- predict(mod$model$model, 
     newdata = expand.grid(d), coef = coef(mod)))
-(p1 <- tmlt(mod, newdata = as.list(d)))
-(p2 <- tmlt(mod, newdata = d, q = d$y[1]))
+(p1 <- predict(mod, newdata = as.list(d)))
+(p2 <- predict(mod, newdata = d, q = d$y[1]))
 
 max(abs(p0 - as.vector(p1)))
 
@@ -26,7 +26,7 @@ all.equal(p1[cbind(1:n, 1:n, 1), drop = TRUE],
 all.equal(p1[cbind(1:n, 1:n, 1:n), drop = TRUE],
           drop(p), check.attributes = FALSE)
 
-qmlt(mod, newdata = list(x1 = 1:3, x2 = 2:3), p = c(.25, .5))
+predict(mod, newdata = list(x1 = 1:3, x2 = 2:3), p = c(.25, .5), type = "quantile")
 
 simulate(mod, nsim = 1, seed = 291, interpolate = FALSE)
 
@@ -39,13 +39,13 @@ r <- as.basis(~ y, data = d, remove_intercept = TRUE,
 
 mod2 <- mlt(model(r, shift = ~ x1 + x2), data = d)
 
-tmlt(mod2, q = unique(d$y))
+predict(mod2, q = unique(d$y))
 
-qmlt(mod2, p = 1:9 / 10)
+predict(mod2, p = 1:9 / 10, type = "quantile")
 
 simulate(mod2, nsim = 3, seed = 29)
 
-dmlt(mod2, q = unique(d$y))
+predict(mod2, q = unique(d$y), type = "density")
 
-dmlt(mod2, list(y = unique(d$y), x1 = 1:3, x2 = 2:3))
+predict(mod2, list(y = unique(d$y), x1 = 1:3, x2 = 2:3), type = "density")
 
