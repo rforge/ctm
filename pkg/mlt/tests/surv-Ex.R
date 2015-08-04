9cc3a08a20c661b600749fc53d9c2add8a5e2db4
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-04 08:31:32 +0000

    handle surv via .surv2R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@284 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/tests/surv-Ex.R
+++ pkg/mlt/tests/surv-Ex.R
@@ -8,60 +8,33 @@ d <- rep(c(TRUE, FALSE), length = length(y))
 ### right
 (s <- Surv(y, d, type = "right"))
 attr(s, "type")
-cbind(unclass(s), mlt:::.Surv2matrix(s))
+cbind(s, mlt:::.Surv2R(s))
 
 ### right censored, left truncated
 (s <- Surv(y, y + 1, d))
 attr(s, "type")
-cbind(unclass(s), mlt:::.Surv2matrix(s))
+cbind(s, mlt:::.Surv2R(s))
 
 ### left censored
 (s <- Surv(y, d, type = "left"))
 attr(s, "type")
-cbind(unclass(s), mlt:::.Surv2matrix(s))
+cbind(s, mlt:::.Surv2R(s))
 
 ### interval
 dd <- rep(0:3, length = length(y))
 (s <- Surv(y, y + 1, dd, type = "interval"))
 attr(s, "type")
-cbind(unclass(s), mlt:::.Surv2matrix(s))
+cbind(s, mlt:::.Surv2R(s))
 
 ### interval2
 (s <- Surv(y, y + 1, type = "interval2"))
 attr(s, "type")
-cbind(unclass(s), mlt:::.Surv2matrix(s))
+cbind(s, mlt:::.Surv2R(s))
 
 (s <- Surv(y, ifelse(d, Inf, y + 1), type = "interval2"))
 attr(s, "type")
-cbind(unclass(s), mlt:::.Surv2matrix(s))
+cbind(s, mlt:::.Surv2R(s))
 
 (s <- Surv(y, ifelse(d, NA, y + 1), type = "interval2"))
 attr(s, "type")
-cbind(unclass(s), mlt:::.Surv2matrix(s))
-
-### new interface
-tm <- runif(10)
-e <- sample(c(TRUE, FALSE), length(tm), replace = TRUE)
-s <- Surv(tm, e)
-cbind(s, as.data.frame(mlt:::.Surv2matrix(s)))
 cbind(s, mlt:::.Surv2R(s))
-
-s <- Surv(tm, e, type = "left")
-cbind(s, as.data.frame(mlt:::.Surv2matrix(s)))
-cbind(s, mlt:::.Surv2R(s))
-
-s <- Surv(tm, tm + 1, type = "interval2")
-cbind(s, as.data.frame(mlt:::.Surv2matrix(s)))
-cbind(s, mlt:::.Surv2R(s))
-
-s <- Surv(tm, tm + 1, sample(0:3, length(tm), replace = TRUE), type = "interval")
-cbind(s, as.data.frame(mlt:::.Surv2matrix(s)))
-cbind(s, mlt:::.Surv2R(s))
-
-s <- Surv(tm, tm + 1, e, type = "counting")
-cbind(s, as.data.frame(mlt:::.Surv2matrix(s)))
-cbind(s, mlt:::.Surv2R(s))
-
-R(gl(3, 2, ordered = TRUE))
-R(0:10)
-
