8b6d6187a4856ddba578ecdbda6823e3d619d87a
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-04 09:30:39 +0000

    cleanup, formula is y | stratum ~ x, allow multiple trafo, add new classes dltm, cltm
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@292 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ltm/R/ltm.R
+++ pkg/ltm/R/ltm.R
@@ -1,59 +1,52 @@
 
 ltm <- function(formula, data, subset, weights, na.action = na.omit,
                 method = c("logistic", "probit", "cloglog"),
-                trafo = c("Bernstein", "Legendre", "fixed log", "log"),
-                integerAsFactor = FALSE, order = 5, support = NULL, 
+                trafo = c("Bernstein", "Legendre", "fixed log", "log", "linear"),
+                integerAsFactor = TRUE, order = 5, support = NULL, 
                 negative_lp = TRUE, contrasts.arg = NULL, ...) {
 
+    ### extract response and possibly a stratum and predictor variables
     if (missing(data))
         data <- environment(formula)
     mf <- match.call(expand.dots = FALSE)
     m <- match(c("formula", "data", "subset", "weights", "na.action"),
                names(mf), 0)
     mf <- mf[c(1, m)]
- 
     formula <- Formula::Formula(formula)
-
-    stopifnot(length(formula)[1] == 1L)   
-    stopifnot(length(formula)[2] %in% 1:2)
-
-    strata <- FALSE
-    if (length(formula)[2] == 2) strata <- TRUE
-
+    ## formula is `response | stratum ~ x'
+    stopifnot(length(formula)[1] %in% 1:2)
+    STRATA <- (length(formula)[1] == 2)
+    stopifnot(length(formula)[2] == 1L)   
     mf$formula <- formula
     mf$drop.unused.levels <- FALSE
     mf$na.action <- na.action
     mf[[1]] <- as.name("model.frame")
     mf <- eval(mf, parent.frame())
     mt <- terms(formula, data = data)
-    mtX <- delete.response(terms(formula, data = data, rhs = 1L))
-    if (strata) {
-        stratum <- Formula::model.part(formula, mf, rhs = 2L)
+    mtX <- delete.response(terms(formula, data = data, lhs = 0, rhs = 1L))
+    if (STRATA) {
+        stratum <- Formula::model.part(formula, mf, lhs = 2L, rhs = 0)
         stopifnot(length(stratum) == 1)
         stopifnot(is.factor(stratum[[1]]) & nlevels(stratum[[1]]) > 1)
     }
     weights <- model.weights(mf)
-    Y <- Formula::model.part(formula, mf, lhs = 1L)
+    Y <- Formula::model.part(formula, mf, lhs = 1L, rhs = 0)
     response <- names(Y)
     Y <- Y[[response]]
-    if (inherits(Y, "Surv")) {
-        Y <- mlt:::.Surv2R(Y)
-        rclass <- "Surv"
-    } else {
-        rclass <- class(Y)[1]
-    }
-    if (inherits(Y, "response")) {
+
+    SURV <- inherits(Y, "Surv")
+    if (!inherits(Y, "response"))
+        Y <- R(Y)
+    type <- attr(Y, "type")
+    DISCRETE <- type != "double" || (type == "integer" && !integerAsFactor)
+    
+    if (!DISCRETE) {
         if (is.null(support)) {
             tmp <- unlist(Y[, c("cleft", "exact", "cright")])
             support <- range(tmp[is.finite(tmp)], na.rm = TRUE)
-            if (rclass == "Surv") support[1] <- 0
-        rclass <- ifelse (all(is.na(Y$exact)), class(Y$cleft), class(Y$exact))
+            if (SURV) support[1] <- 0
         }
-    } else {
-        if (is.null(support))
-            support <- range(Y[is.finite(Y)])
-    }
-    rtype <- c("continuous", "discrete")[rclass %in% c("factor", "integer") + 1L]
+    } 
 
     method <- match.arg(method)
     todistr <- switch(method, "logistic" = "Logistic",
@@ -61,39 +54,49 @@ ltm <- function(formula, data, subset, weights, na.action = na.omit,
                               "cloglog" = "MinExtrVal")
 
     fixed <- NULL
-    if (rtype == "discrete" & ifelse(rclass == "integer", integerAsFactor, TRUE)) {
-        if (rclass == "integer") Y <- ordered(Y)
+    if (DISCRETE) {
+        if (type == "integer") Y <- ordered(Y)
         nlev <- nlevels(Y)
         cntr <- list(function(n) contr.treatment(n, base = nlev))
         names(cntr) <- response
         rtrafo <- as.basis(as.formula(paste("~", response)), data = mf, remove_intercept = TRUE,
               contrasts.arg = cntr, ui = diff(diag(nlev - 1)), ci = 0)
     } else {
-        trafo <- match.arg(trafo)
-        rtrafo <- switch(trafo, 
+        if (is.null(match.call()$trafo)) trafo <- "Bernstein"
+        trafo <- match.arg(trafo, several.ok = TRUE)
+        rtrafo <- lapply(trafo, function(tr) switch(tr, 
             "fixed log" = log_basis(ui = "increasing", varname = response, support = support),
             "log" = log_basis(ui = "increasing", varname = response, support = support),
             "Bernstein" = Bernstein_basis(order = order, support = support,
                           ui = "increasing", varname = response),
             "Legendre" = Legendre_basis(order = order, support = support,
-                          ui = "increasing", varname = response))
-        if (trafo == "fixed log") {
+                          ui = "increasing", varname = response),
+            "linear" = polynomial_basis(c(TRUE, TRUE), var = response, 
+                                        support = support, ci = c(-Inf, 0)))
+        )
+        if ("fixed log" %in% trafo) {
             fixed <- 1
-            names(fixed) <- names(model.matrix(rtrafo, data = mf))
+            names(fixed) <- names(model.matrix(rtrafo[["fixed log"]], data = mf))
+        }
+        if (length(rtrafo) > 1) {
+            rtrafo <- do.call("c", rtrafo)
+        } else {
+            rtrafo <- rtrafo[[1L]]
         }
     }
 
     strafo <- xtrafo <- NULL
-    if (strata)
+    if (STRATA)
         strafo <- as.basis(as.formula(paste("~ ", names(stratum), "- 1")), data = mf)
     xtrafo <- as.basis(mtX, data = mf, remove_intercept = TRUE, contrasts.arg = contrasts.arg,
                        negative = negative_lp)
     if (ncol(model.matrix(xtrafo, data = mf)) == 0) xtrafo <- NULL
-
     m <- model(rtrafo, interacting = strafo, shifting = xtrafo, todistr = todistr)
 
     ret <- mlt(m, data = mf, fixed = fixed, scale = TRUE, check = FALSE, ...)
-    class(ret) <- c("ltm", class(ret))
+    ret$call <- match.call(expand.dots = TRUE)
+
+    class(ret) <- c(ifelse(DISCRETE, "dltm", "cltm"), "ltm", class(ret))
     ret
 }
 
