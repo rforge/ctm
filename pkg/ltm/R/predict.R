cf383713d81ba2612bb52b6515a1db9e7c3f9790
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-05 13:25:58 +0000

    faster method for type = lp
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@304 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ltm/R/predict.R
+++ pkg/ltm/R/predict.R
@@ -3,7 +3,8 @@ predict.ltm <- function(object, newdata, type = c("lp", "trafo", "distribution",
     "survivor", "density", "logdensity", "hazard", "loghazard", "cumhazard", "quantile"),
     q = NULL, n = 50, ...) {
 
-    class(object) <- class(object)[-1]
+    cf <- coef(object, lp_only = TRUE)
+    object <- as.mlt(object)
 
     if (missing(newdata))
         newdata <- object$data
@@ -12,13 +13,13 @@ predict.ltm <- function(object, newdata, type = c("lp", "trafo", "distribution",
     type <- match.arg(type)
     if (type == "lp") {
         if (!is.null(q)) warning("argument q ignored")
-        q <- mkgrid(object, n = 1)[[object$response]] 
-        return(drop(predict(object, newdata = newdata, 
-                            type = "trafo", terms = "bshifting", q = q)))
+        if (is.null(cf)) return(NULL)
+        return(drop(predict(object$model$model$bshifting, newdata = newdata, 
+                            coef = cf)))
     }
 
     if (is.null(q))
-        q <- mkgrid(object, n = n)[[object$response]]
+        q <- mkgrid(object, n = n, bounds = object$bounds)[[object$response]]
 
     predict(object, newdata = newdata, type = type, q = q, ...)
 }
