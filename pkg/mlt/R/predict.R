536b6ff3fc7e1b67b5119815442d25dff799c830
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-05 13:25:16 +0000

    terms = all is not necessary
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@303 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/R/predict.R
+++ pkg/mlt/R/predict.R
@@ -1,16 +1,11 @@
 
 predict.mlt <- function(object, newdata = object$data, type = c("trafo", "distribution", "survivor", 
     "density", "logdensity", "hazard", "loghazard", "cumhazard", "quantile"), 
-    terms = c("all", "bresponse", "binteracting", "bshifting"), q = NULL, p = NULL, n = 50,
+    terms = c("bresponse", "binteracting", "bshifting"), q = NULL, p = NULL, n = 50,
     interpolate = TRUE, ...) {
 
     type <- match.arg(type)
-    terms <- match.arg(terms)
-    if (terms != "all") {
-        stopifnot(type == "trafo")
-    } else {
-       terms <- NULL
-    }
+    terms <- match.arg(terms, several.ok = TRUE)
 
     if (type == "quantile")
         stopifnot(!is.null(p) && (min(p) > 0 & max(p) < 1))
