69999ba58f3418f21783110d8f8c395a7fe7b7cd
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-05 13:26:59 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@308 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ltm/tests/ltm.R
+++ pkg/ltm/tests/ltm.R
@@ -2,40 +2,56 @@
 library("ltm")
 library("multcomp")
 
-coef(a <- ltm(Sepal.Length | Species ~ Sepal.Width, data = iris, trafo = "log"))
+(a <- ltm(Sepal.Length | Species ~ Sepal.Width, data = iris, trafo = "log"))
+summary(a)
+coef(a)
+vcov(a)
 predict(a)
 
 library("survival")
 data("GBSG2", package = "TH.data")
 storage.mode(GBSG2$time) <- "double"
 
-b <- ltm(time ~ horTh , data = GBSG2)
-
+(b <- ltm(time ~ horTh , data = GBSG2))
+summary(b)
+coef(b)
+vcov(b)
 predict(b)
+predict(b, newdata = data.frame(horTh = unique(GBSG2$horTh)), q = 500:510, type = "trafo")
 
-predict(b, newdata = data.frame(horTh = unique(GBSG2$horTh)), q = 100:110, type = "trafo")
+(b <- ltm(time ~ 1, data = GBSG2))
+summary(b)
+coef(b)
+vcov(b)
+predict(b)
+predict(b, q = 500:510, type = "distribution")
 
-b <- ltm(time ~ 1, data = GBSG2)
+(b <- ltm(time | tgrade ~ 1, data = GBSG2))
+summary(b)
+coef(b)
+vcov(b)
+predict(b)
+predict(b, newdata = data.frame(tgrade = unique(GBSG2$tgrade)), q = 500:510, type = "distribution")
 
-predict(b, q = 100:110, type = "distribution")
-
-b <- ltm(time | tgrade ~ 1, data = GBSG2)
-
-predict(b, newdata = data.frame(tgrade = unique(GBSG2$tgrade)), q = 100:110, type = "distribution")
-
-cc <- ltm(Surv(time, cens) | tgrade ~ horTh + menostat + pnodes, data = GBSG2, method = "cloglog")
-
+(cc <- ltm(Surv(time, cens) | tgrade ~ horTh + menostat + pnodes, data = GBSG2, method = "cloglog"))
+summary(cc)
+coef(cc)
+vcov(cc)
+predict(cc)
 predict(cc, newdata = expand.grid(horTh = c("no", "yes"), menostat = "Pre",
-                           pnodes = 100, tgrade = "II"), q = 100:101, type = "trafo")
+                           pnodes = 100, tgrade = "II"), q = 500:501, type = "trafo")
 
-d <- ltm(Surv(time, cens) ~ 1, data = GBSG2, method = "cloglog")
+(d <- ltm(Surv(time, cens) ~ 1, data = GBSG2, method = "cloglog"))
+summary(d)
+coef(d)
+vcov(d)
+predict(d)
 
 confint(d, calpha = univariate_calpha())
 
-class(d) <- class(d)[-(1:2)]
+d <- as.mlt(d)
 cf <- coef(d)
 v <- vcov(d)
-
 
 prm <- parm(cf, v)
 K <- diag(length(cf))
@@ -43,9 +59,7 @@ rownames(K) <- colnames(K) <- names(cf)
 ci <- confint(glht(prm, linfct = K), calpha = qnorm(.975))
 
 lwr <- d
-class(lwr) <- class(lwr)[-(1:2)]
 upr <- d
-class(upr) <- class(upr)[-(1:2)]
 coef(lwr) <- ci$confint[, "lwr"]
 coef(upr) <- ci$confint[, "upr"]
 
