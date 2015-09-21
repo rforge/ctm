2490d2af1ad2bdb3a181712153428388e7e6f9f4
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-09-21 13:40:14 +0000

    use mlt::confband when possible
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@341 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ltm/R/confint.R
+++ pkg/ltm/R/confint.R
@@ -17,27 +17,5 @@ confint.ltm <- function(object, parm, level = 0.95,
                        level = level, ...))
     } 
 
-    ### confint.mlt?
-    stopifnot(is.data.frame(parm))
-    if (nrow(parm) > 1) {
-        ret <- lapply(1:nrow(parm), function(i) 
-            confint(object = object, parm = parm[i,,drop = FALSE], 
-                    level = level, type = type, ...))
-        return(ret)
-    }
-
-    stopifnot(nrow(parm) == 1)
-    y <- object$response
-    q <- mkgrid(object, n = n)[[y]]
-    parm <- parm[rep(1, length(q)),,drop = FALSE]
-    parm[[y]] <- q
-    X <- model.matrix(object$model$model, data = parm)
-    ci <- confint(glht(parm(coef(object, lp_only = FALSE), 
-                            vcov(object, lp_only = FALSE)), 
-                       linfct = X), ...)$confint
-    if (type == "distribution") ci <- object$model$todistr$p(ci)
-    if (type == "survivor") ci <- 1 - object$model$todistr$p(ci)
-    if (type == "cumhazard") ci <- -log(1 - object$model$todistr$p(ci))
-    ci <- cbind(q = q, ci)
-    return(ci)
+    mlt::confband(as.mlt(object), newdata = parm, level = level, type = type, n = n, ...)
 }
