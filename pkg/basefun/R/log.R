be37454fe4ae7667e1edf16040d160c349e20526
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-12-04 09:56:27 +0000

    allow intercepts in log_basis
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@400 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/basefun/R/log.R
+++ pkg/basefun/R/log.R
@@ -1,6 +1,6 @@
 
-log_basis <- function(var, 
-                      ui = c("none", "increasing", "decreasing")) {
+log_basis <- function(var, ui = c("none", "increasing", "decreasing"), 
+                      remove_intercept = TRUE) {
 
     stopifnot(inherits(var, "numeric_var"))
     varname <- variable.names(var)
@@ -12,6 +12,10 @@ log_basis <- function(var,
     ui <- switch(ui, "none" = matrix(1),
                      "increasing" = matrix(1),
                      "decreasing" = matrix(-1))
+    if (!remove_intercept) {
+        ci <- c(-Inf, ci)
+        ui <- cbind(0, ci)
+    }
 
     basis <- function(data, deriv = 0L) {
 
@@ -23,22 +27,27 @@ log_basis <- function(var,
             x <- data[[varname]]
         }
         if (deriv == 0) {
-            X <- matrix(log(pmax(x, .Machine$double.eps)), ncol = 1)
+            X <- cbind(1, matrix(log(pmax(x, .Machine$double.eps)), ncol = 1))
         } else if (deriv == 1) {
-            X <- matrix(1 / x, ncol = 1)
+            X <- cbind(0, matrix(1 / x, ncol = 1))
         } else if (deriv > 1) {
             stop("not yet implemented")
         } else if (deriv < 0) {
-            X <- matrix(0, ncol = 1, nrow = length(x))
+            X <- cbind(0, matrix(0, ncol = 1, nrow = length(x)))
         }
-        colnames(X) <- paste("log(", varname, ")", sep = "")
+        colnames(X) <- c("(Intercept)", paste("log(", varname, ")", sep = ""))
+        if (remove_intercept) {
+            X <- X[, -1L, drop = FALSE]
+        } else {
+            varname <- c("(Intercept)", varname)
+        }
         attr(X, "constraint") <- list(ui = ui, ci = ci)
         attr(X, "Assign") <- varname
         X
     }
 
     attr(basis, "variables") <- var
-    attr(basis, "intercept") <- FALSE
+    attr(basis, "intercept") <- !remove_intercept
 
     class(basis) <- c("log_basis", "basis", class(basis))
     return(basis)
