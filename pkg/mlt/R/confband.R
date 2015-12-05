6edd676413c05be0f47f253f872ee0d152b9a435
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-12-05 21:00:16 +0000

    add quantile smoothing for confbands (ncheat > n argument)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@401 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/R/confband.R
+++ pkg/mlt/R/confband.R
@@ -3,7 +3,8 @@ confband <- function(object, newdata, level = .95, ...)
     UseMethod("confband")
 
 confband.mlt <- function(object, newdata, level = 0.95,
-                        type = c("trafo", "distribution", "survivor", "cumhazard"), n = 20, ...) {
+                         type = c("trafo", "distribution", "survivor", "cumhazard"), 
+                         n = 20, ncheat = n, ...) {
 
     stopifnot(!missing(newdata))
     stopifnot(is.data.frame(newdata))
@@ -18,11 +19,16 @@ confband.mlt <- function(object, newdata, level = 0.95,
     stopifnot(nrow(newdata) == 1)
     y <- object$response
     q <- mkgrid(object, n = n)[[y]]
-    newdata <- newdata[rep(1, length(q)),,drop = FALSE]
-    newdata[[y]] <- q
-    X <- model.matrix(object$model$model, data = newdata)
+    nd <- newdata[rep(1, length(q)),,drop = FALSE]
+    nd[[y]] <- q
+    X <- model.matrix(object$model$model, data = nd)
     ci <- confint(multcomp::glht(multcomp::parm(coef(object), vcov(object)),
                                  linfct = X), ...)$confint
+    ### use quantile obtained for n contrasts for larger number
+    ### of contrasts
+    if (ncheat > n)
+        return(confband(object = object, newdata = newdata, level = level,
+                        type = type, n = ncheat, calpha = attr(ci, "calpha")))
     if (type == "distribution") ci <- object$model$todistr$p(ci)
     if (type == "survivor") ci <- 1 - object$model$todistr$p(ci)
     if (type == "cumhazard") ci <- -log(1 - object$model$todistr$p(ci))
