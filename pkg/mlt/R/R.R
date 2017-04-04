b3ab07194e646a9a05f2b0827462a333717aead4
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-04-04 12:29:36 +0000

    0.1-3
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@724 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/R/R.R
+++ pkg/mlt/R/R.R
@@ -52,6 +52,8 @@ R.Surv <- function(object, ...) {
         function(y) {
             uy <- sort(unique(y))
             s <- summary(sf, times = uy)$surv
+            if (length(s) < length(uy))
+                s <- c(s, rep(0, length(uy) - length(s)))
             s[is.na(s)] <- 0
             p <- 1 - s
             p[match(y, uy)]
