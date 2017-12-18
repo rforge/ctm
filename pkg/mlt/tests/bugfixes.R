4a70ec9304da9c4409b68fe43173fa3e711d7c46
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-12-18 10:59:22 +0000

    basefun 0.0-39 with intercept_basis
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@802 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/tests/bugfixes.R
+++ pkg/mlt/tests/bugfixes.R
@@ -57,3 +57,17 @@ library("survival")
 bodyfat$DEXfat <- Surv(y, sF)
 mod <- mlt(m, data = bodyfat, scale = TRUE, checkGrad = FALSE)
 mod$convergence
+
+### just in case: check for new intercept_basis (basefun 0.0-39)
+d <- data.frame(y = rnorm(100, mean = 2, sd = .25))
+m <- ctm(as.basis(~ y, data = d, remove_intercept = TRUE, ui = matrix(1), ci = 0), 
+         shifting = intercept_basis(), todistr = "Normal")
+f <- mlt(m, data = d, scale = TRUE)
+
+m2 <- ctm(as.basis(~ y, data = d, ui = matrix(c(0, 1), nr = 1), ci = 0), todistr = "Normal")
+f2 <- mlt(m2, data = d, scale = TRUE)
+stopifnot(all.equal(coef(f), coef(f2)[2:1]))
+stopifnot(all.equal(logLik(f), logLik(f2)))
+stopifnot(all.equal(estfun(f), estfun(f2)[,2:1]))
+
+
