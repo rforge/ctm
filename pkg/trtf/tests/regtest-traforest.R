2d6b0e6b49a9a6876c1fe5d5372faec66b2f1287
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-06-23 09:02:17 +0000

    no mclappyl on Windows
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@962 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/trtf/tests/regtest-traforest.R
+++ pkg/trtf/tests/regtest-traforest.R
@@ -48,20 +48,23 @@ cmod <- coxph(Surv(time, cens) ~ horTh + age + menostat + tsize + tgrade +
 
 plot(survfit(cmod, newdata = GBSG2[1:3,]))
 
-### sanity checks
+### sanity checks; no mclapply on Windows
+if (.Platform$OS.type != "windows") {
 
-p11 <- predict(tf, newdata = GBSG2[1:11,], type = "weights")
-p12 <- predict(tf, newdata = GBSG2[1:11,], type = "weights", cores = 2)
-stopifnot(all.equal(p11, p12))
+  p11 <- predict(tf, newdata = GBSG2[1:11,], type = "weights")
+  p12 <- predict(tf, newdata = GBSG2[1:11,], type = "weights", cores = 2)
+  stopifnot(all.equal(p11, p12))
 
-p21 <- predict(tf, newdata = GBSG2[1:11,], type = "node")
-p22 <- predict(tf, newdata = GBSG2[1:11,], type = "node", cores = 2)
-stopifnot(all.equal(p21, p22))
+  p21 <- predict(tf, newdata = GBSG2[1:11,], type = "node")
+  p22 <- predict(tf, newdata = GBSG2[1:11,], type = "node", cores = 2)
+  stopifnot(all.equal(p21, p22))
 
-p31 <- predict(tf, newdata = GBSG2[1:11,])
-p32 <- predict(tf, newdata = GBSG2[1:11,], cores = 2)
-stopifnot(all.equal(p31, p32))
+  p31 <- predict(tf, newdata = GBSG2[1:11,])
+  p32 <- predict(tf, newdata = GBSG2[1:11,], cores = 2)
+  stopifnot(all.equal(p31, p32))
 
-p41 <- predict(tf, newdata = GBSG2[1:11,], type = "coef")
-p42 <- predict(tf, newdata = GBSG2[1:11,], cores = 2, type = "coef")
-stopifnot(all.equal(p41, p42))
+  p41 <- predict(tf, newdata = GBSG2[1:11,], type = "coef")
+  p42 <- predict(tf, newdata = GBSG2[1:11,], cores = 2, type = "coef")
+  stopifnot(all.equal(p41, p42))
+
+}
