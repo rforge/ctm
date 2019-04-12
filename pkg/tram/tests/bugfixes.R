571d005d97bbb1d4c7a1e0b3a7d0429cc3f26543
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-04-12 13:09:19 +0000

    add test for count data and log_first = TRUE
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@1054 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tram/tests/bugfixes.R
+++ pkg/tram/tests/bugfixes.R
@@ -15,3 +15,55 @@ stopifnot(all.equal(s0[,"(Intercept)"], -s2[,"int"]))
 stopifnot(all.equal(s1[,"(Intercept)"], -s2[,"int"]))
 stopifnot(all.equal(s2[,"(Intercept)"], -s2[,"int"]))
 
+### log_first for count data (by Sandra Siegfried)
+### use: y + 1; log_first = TRUE, support = c(1, ...), bounds = c(1, ...)
+library("MASS")
+set.seed(29)
+Nsim <- 100
+b1 <- - 4.5
+b0 <- 5
+theta <- 2
+h <- qlogis(ppois(0:100, lambda = 5))
+dgp <- function(n = 4000){
+  x <- runif(n, min = 0, max = 1) 
+  log.mu <- b0 + b1 * x
+  h.m <- matrix(h, nrow = length(h), ncol = length(x)) 
+  p <- (plogis(t(h.m) - b1 * x) - runif(n))^2
+  y <- max.col(-p) - 1
+  m <- Colr(y ~ x, data = data.frame(y = y, x = x), 
+            bounds = c(0L, Inf), fixed = c("x" = -b1),
+            support = c(0L, floor(quantile(y, .9))), order = 10)
+  
+  ret <- data.frame(x = x, y = as.integer(y))
+  attr(ret, "mC") <- m
+  ret
+}
+d <- dgp()
+d$y.p1 <- d$y + 1L
+
+m1 <- Colr(y ~ x, data = d,
+                  support = c(0L, as.numeric(max(d$y))),
+                  bounds = c(0L, Inf),
+                  order = 10)
+
+try(m2a <- Colr(y ~ x, data = d,
+                  support = c(1L, as.numeric(max(d$y))),
+                  bounds = c(1L, Inf),
+                  log_first = TRUE,
+                  order = 10))
+
+try(m2b <- Colr(y ~ x, data = d,
+                  support = c(1L, as.numeric(max(d$y))),
+                  bounds = c(0L, Inf),
+                  log_first = TRUE,
+                  order = 10))
+
+m3 <- Colr(y.p1 ~ x, data = d,
+                  support = c(1L, as.numeric(max(d$y.p1))),
+                  bounds = c(1L, Inf),
+                  log_first = TRUE,
+                  order = 10)
+
+l1 <- m1$logliki(coef(as.mlt(m1)), rep(1, nrow(d)))
+l3 <- m3$logliki(coef(as.mlt(m3)), rep(1, nrow(d)))
+stopifnot(cor(l1, l3) > .96)
