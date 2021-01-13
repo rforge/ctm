7ca143298e4b32e237a6fa6a6c8365e042217691
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-01-13 15:39:49 +0000

    start inversion of cdf by spline/approx for simulate
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@1531 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlt/R/sample.R
+++ pkg/mlt/R/sample.R
@@ -93,3 +93,121 @@ paraboot.mlt <- function(object, parm = coef(object), B = 100, ...) {
     class(object) <- c("mlt_paraboot", class(object))
     object
 }
+
+sim <- function(object, nsim = 1, seed = NULL, 
+                newdata, K = 50, q = NULL,
+                bysim = TRUE, ...) {
+
+    ### from stats:::simulate.lm
+    if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE)) 
+        runif(1)
+    if (is.null(seed)) 
+        RNGstate <- get(".Random.seed", envir = .GlobalEnv)
+    else {
+        R.seed <- get(".Random.seed", envir = .GlobalEnv)
+        set.seed(seed)
+        RNGstate <- structure(seed, kind = as.list(RNGkind()))
+        on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
+    }
+
+    if (is.data.frame(newdata)) {
+        U <- matrix(runif(nsim * NROW(newdata)), nrow = nsim, 
+                    byrow = TRUE) ### be compatible with mlt < 1.2-1
+        U <- pmax(U, sqrt(.Machine$double.eps))
+        U <- pmin(U, 1 - sqrt(.Machine$double.eps))
+
+    } else {
+        stop("not yet implemented")
+    }
+
+    y <- variable.names(object, "response")
+    if (is.null(q))
+        q <- mkgrid(object, n = K)[[y]]
+
+    ret <- .p2q2(object = object, newdata = newdata, p = U, q = q)
+
+    if (nsim > 1) {
+        if (bysim) {
+            tmp <- vector(mode = "list", length = nsim)
+            for (i in 1:nsim) {
+                idx <- 1:NROW(newdata) + (i - 1) * NROW(newdata)
+                if (is.data.frame(ret)) 
+                    tmp[[i]] <- ret[idx,]
+                else 
+                    tmp[[i]] <- ret[idx]
+            }
+        } else {
+            tmp <- vector(mode = "list", length = NROW(newdata))
+            for (i in 1:NROW(newdata)) {
+                idx <- NROW(newdata) * 0:(nsim - 1) + i
+                if (is.data.frame(ret)) 
+                    tmp[[i]] <- ret[idx,]
+                else 
+                    tmp[[i]] <- ret[idx]
+            }
+        }
+        ret <- tmp
+    }
+    return(ret)
+}
+
+### numerically invert distribution function
+.p2q2 <- function(object, newdata, p, q) {
+
+    stopifnot(!missing(newdata))
+
+    ### basically compute quantiles for p; see qmlt
+    prob <- predict(object, newdata = newdata, q = q, type = "distribution")
+    ### unconditional: newdata is ignored in the presence of q
+    if (!is.matrix(prob))
+        prob <- matrix(prob, ncol = 1)[,rep(1, NROW(newdata)), drop = FALSE]
+    y <- variable.names(object, "response")
+    bounds <- bounds(object)[[y]]
+
+    discrete <- !inherits(as.vars(object)[[y]],
+                          "continuous_var")
+    if (discrete) {
+        ### use "old" code
+        prob <- t(prob[, rep(1:NCOL(prob), nrow(p)),drop = FALSE])
+        prob <- cbind(0, prob, 1)
+        i <- rowSums(prob < as.vector(t(p)))
+        return(q[i])
+    }
+
+    ### use spline/approx to evaluate quantile function
+    nleft <- nexact <- nright <- 
+        matrix(NA, nrow = nrow(p), ncol = NROW(newdata))
+
+    for (i in 1:NCOL(prob)) {
+             
+        pr <- prob[,i]
+        pr0 <- which(pr < sqrt(.Machine$double.eps))
+        pr1 <- which(pr > 1 - sqrt(.Machine$double.eps))
+        rmi <- c()
+        if (length(pr0) > 0L)
+            rmi <- c(rmi, pr0[-length(pr0)])
+        if (length(pr1) > 0L)
+            rmi <- c(rmi, pr1[-1L])
+        if (length(rmi) > 1L) {
+            qq <- q[-rmi]
+            pr <- pr[-rmi]
+        } else {
+            qq <- q
+        }
+        s <- spline(x = qq, y = pr, method = "hyman")
+        ynew <- approx(x = s$y, y = s$x, xout = p[,i], 
+                       yleft = -Inf, yright = Inf)$y
+        nleft[ynew == -Inf, i] <- bounds[1L]
+        nright[ynew == -Inf, i] <- min(q)
+        nleft[ynew == Inf, i] <- max(q)
+        nright[ynew == Inf, i] <- bounds[2L]
+        ynew[!is.finite(ynew)] <- NA
+        nexact[, i] <- ynew
+    }
+
+    if (all(is.na(nleft)) && all(is.na(nright)))
+        return(as.vector(t(nexact)))
+    return(R(as.vector(t(nexact)), 
+             cleft = as.vector(t(nleft)), 
+             cright = as.vector(t(nright))))
+}
