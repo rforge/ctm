ec28f2fc39b3b1ad89324b2c5c5ed4650f07b866
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-02-05 14:46:00 +0000

    use last event time as max(support)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@1580 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tram/R/internal.R
+++ pkg/tram/R/internal.R
@@ -25,9 +25,10 @@ asvar.numeric <- function(object, name, prob = c(.1, .9), support = NULL, bounds
 asvar.Surv <- function(object, name, prob = c(.1, .9), support = NULL, bounds = c(0, Inf), add = c(0, 0), ...) {
     if (is.null(bounds)) bounds <- c(0, Inf)
     if (is.null(support)) {
-        support <- quantile(survfit(y ~ 1, data = data.frame(y = object)), prob = prob)$quantile
+        support <- quantile(sf <- survfit(y ~ 1, data = data.frame(y = object)), prob = prob)$quantile
+        last <- max(sf$time[sf$n.event > 0])
         if (is.na(support[2])) 
-            support[2] <- max(object[, 1])
+            support[2] <- last
         add <- c(0 - support[1], 0)
         return(variables::numeric_var(name, support = support, bounds = bounds, add = add, ...))
     }
